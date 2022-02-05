aedb4d81863666aac26b09b0a669471dc4f4afc8
davidwpelo <65370511+davidwpelo@users.noreply.github.com>
2022-02-05 15:17:59 -0600

    Added server data and refactored the way contacts are rendered

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,57 +2,23 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar";
 import ContactList from "./components/ContactList";
 import AddForm from "./components/AddForm";
-import axios from 'axios';
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-    { name: "David Pelo", number: "39-89-6423122", id: 5 },
-  ]);
-
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3002/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-      })
-  }, [])
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [contactList, setContactList] = useState(
-    persons.map((person) => (
-      <p key={person.id}>
-        {person.name} {person.number}
-      </p>
-    ))
-  );
+  const [searchBy, setSearchBy] = useState("");
+
+  const getPersons = () => {
+    axios.get("http://localhost:3002/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(getPersons, []);
 
   const searchHandler = (e) => {
-    const searchValue = e.target.value;
-
-    if (searchValue.trim().length > 0) {
-      const filteredContactList = persons
-        .filter((person) => person.name.indexOf(searchValue) > -1)
-        .map((person) => (
-          <p key={person.id}>
-            {person.name} {person.number}
-          </p>
-        ));
-      setContactList(filteredContactList);
-    } else {
-      setContactList(
-        persons.map((person) => (
-          <p key={person.id}>
-            {person.name} {person.number}
-          </p>
-        ))
-      );
-    }
+    setSearchBy(e.target.value);
   };
 
   const nameChangeHandler = (e) => {
@@ -87,18 +53,24 @@ const App = () => {
       id: persons.length + 1,
     };
 
-    let newList = persons.concat(newNameObject);
     setPersons(persons.concat(newNameObject));
-    setContactList(
-      newList.map((person) => (
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const contacts = !searchBy
+    ? persons.map((person) => (
         <p key={person.id}>
           {person.name} {person.number}
         </p>
       ))
-    );
-    setNewName("");
-    setNewNumber("");
-  };
+    : persons
+        .filter((person) => person.name.indexOf(searchBy) > -1)
+        .map((person) => (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ));
 
   return (
     <div>
@@ -113,7 +85,7 @@ const App = () => {
         onSubmit={addNewNameHandler}
       />
       <h2>Numbers</h2>
-      <ContactList contacts={contactList} />
+      <ContactList contacts={contacts} />
     </div>
   );
 };
