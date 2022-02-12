6d3c262e4f4dcd36491781db15424a4660b20399
davidwpelo <65370511+davidwpelo@users.noreply.github.com>
2022-02-12 15:53:54 -0600

    Completed Part 2e

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,83 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import Note from './components/Note'
-import noteService from './services/notes'
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(false)
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    noteService
-      .getAll()
-      .then(initialNotes => {
-        setNotes(initialNotes);
-      });
-  }, [])
-  console.log('render', notes.length, 'notes')
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
-  const addNote = event => {
-    event.preventDefault()
+  const addNote = (event) => {
+    event.preventDefault();
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-    }
+    };
 
-    noteService
-      .create(noteObject)
-      .then(newNote => {
-        setNotes(notes.concat(newNote))
-        setNewNote('');
-      });
-  }
+    noteService.create(noteObject).then((newNote) => {
+      setNotes(notes.concat(newNote));
+      setNewNote("");
+    });
+  };
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
 
   const toggleImportanceOf = (id) => {
-    const note = notes.find(n => n.id === id);
+    const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
     noteService
       .update(id, changedNote)
-      .then(updatedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : updatedNote))
+      .then((updatedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : updatedNote)));
       })
       .catch(error => {
-        alert(`the note '${note.content}' was already deleted from server`);
-        setNotes(notes.filter(n => n.id !== id))
-      });
-  }
+        setErrorMessage(
+          `the note '${note.content}' was already deleted from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
 
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
-      </div>   
+      </div>
       <ul>
-        {notesToShow.map(note => 
-            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
-        )}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
+        <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
-      </form>  
+      </form>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
